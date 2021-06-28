dd35725b6190a04aa9784dff804daabd6093c5f3
Appriou Jérémy <jeremy.appriou@gmail.com>
2021-06-28 10:57:46 +0200

    edited build and watch script + better include of node_modules for bouledogue.scss

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("ouais");
+function build() {
+  console.log("Building bouledogue...");
+  var sass = require("node-sass");
+  sass.render({
+    file: "./scss/bouledogue.scss",
+  });
+  console.log("done building bouledogue, bark 🐶");
+}
+
+for (var i = 0; i < process.argv.length; i++) {
+  switch (process.argv[i]) {
+    case "build":
+      build();
+      break;
+    case "watch":
+      watch();
+      break;
+  }
+}
