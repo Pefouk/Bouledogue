ca95382d5434cd8ed31ca3c7caf9e6b67339d2c5
Appriou Jérémy <jeremy.appriou@gmail.com>
2021-07-02 16:41:23 +0200

    building CSS with index.js -> Beginning

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function build() {
+  let fs = require("fs");
+  let sass = require("node-sass");
   console.log("Building bouledogue...");
-  var sass = require("node-sass");
-  sass.render({
+  let css = sass.renderSync({
     file: "./scss/bouledogue.scss",
+    sourceMap: false,
+    outFile: "bouledogue.css",
+    includePaths: ["node_modules/systematize/"],
+    outputStyle: "compressed"
   });
+  console.log("moving to ./css/ ...");
+  fs.writeFileSync('./css/bouledogue.css', css.css);
   console.log("done building bouledogue, bark 🐶");
 }
 
